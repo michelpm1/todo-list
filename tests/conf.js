eeb337970db0a24936c401f8054e3f2ed3dd8f61
michelpm1 <michelpm1@gmail.com>
2016-06-17 00:47:56 -0300

    cover2

--- tests/conf.js
+++ tests/conf.js
@@ -2,4 +2,14 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['todo-spec.js']
+};
+
+if (process.env.TRAVIS) {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    };
 }
